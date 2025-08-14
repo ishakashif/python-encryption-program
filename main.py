f004a00e17dfdb6310ba4bf120026105a21dd244
Isha Kashif <ishak@udel.edu>
2025-08-14 12:56:52 -0400

    encryption completed

--- main.py
+++ main.py
@@ -7,9 +7,18 @@ chars = " " + string.punctuation + string.digits + string.ascii_letters
 chars = list(chars)
 
 key = chars.copy()
-print(f"chars: {chars}")
-print(f"key: {chars}")
+# print(f"chars: {chars}")
+# print(f"key: {chars}")
 
 random.shuffle(key)
 
-#j
+#ENCRYPT
+plain_text = input("Enter a message to encrypt: ")
+cipher_text = ""
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"Original message: {plain_text}")
+print(f"Original message: {cipher_text}")
